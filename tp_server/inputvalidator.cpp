2e6d89112216b6e3c9e4ce02941f410d5750a110
FLEJOSNE <florian.lejosne@ynov.com>
2023-04-22 15:00:14 +0200

    debugging lexer

--- tp_server/inputvalidator.cpp
+++ tp_server/inputvalidator.cpp
@@ -9,23 +9,50 @@ bool validateFileName(const QString& fileName) {
 }
 
 // Vérifie la validité du type de fichier
-bool validateFileType(const QString& fileType) {
+bool validateFileType(const QString& fileType, const QString& fileExtension) {
     // Code de validation de type de fichier
-    return fileType == "image" || fileType == "text" || fileType == "exec";
+    if (fileType == "exec") {
+        return fileExtension == ".sh" || fileExtension == ".jar";
+    } else if (fileType == "text") {
+        return fileExtension == ".txt" || fileExtension == ".pdf" || fileExtension == ".doc" || fileExtension == ".docx";
+    } else if (fileType == "image") {
+        return fileExtension == ".png" || fileExtension == ".jpg" || fileExtension == ".gif" || fileExtension == ".bmp" || fileExtension == ".tif";
+    } else if (fileType == "audio") {
+        return fileExtension == ".mp3";
+    } else if (fileType == "video") {
+        return fileExtension == ".mp4" || fileExtension == ".avi" || fileExtension == ".mov" || fileExtension == ".wmv";
+    } else if (fileType == "archive") {
+        return fileExtension == ".zip" || fileExtension == ".rar" || fileExtension == ".7z" || fileExtension == ".tar" || fileExtension == ".gz";
+    } else {
+        return true;
+    }
 }
 
 // Vérifie la validité de l'extension de fichier
 bool validateFileExtension(const QString& fileExtension) {
     // Code de validation d'extension de fichier
-    return fileExtension == ".sh" || fileExtension == ".txt" || fileExtension == ".png" || fileExtension == ".jpg";
+    return fileExtension == ".sh" || fileExtension == ".jar" || fileExtension == ".txt" ||
+           fileExtension == ".png" || fileExtension == ".jpg" || fileExtension == ".pdf" ||
+           fileExtension == ".doc" || fileExtension == ".docx" || fileExtension == ".xls" ||
+           fileExtension == ".xlsx" || fileExtension == ".ppt" || fileExtension == ".pptx" ||
+           fileExtension == ".mp3" || fileExtension == ".mp4" || fileExtension == ".avi" ||
+           fileExtension == ".mov" || fileExtension == ".wmv" || fileExtension == ".gif" ||
+           fileExtension == ".bmp" || fileExtension == ".tif" || fileExtension == ".zip" ||
+           fileExtension == ".rar" || fileExtension == ".7z" || fileExtension == ".tar" || fileExtension == ".gz";
 }
 
-// Vérifie la validité de la date
-bool validateDate(const QString& date) {
-    // Code de validation de date
-    QDateTime dateTime = QDateTime::fromString(date, "yyyy-MM-dd");
-    return dateTime.isValid();
+
+bool validateDate(const QString& minDate, const QString& maxDate) {
+    // Vérifie la validité des dates
+    QDateTime minDateTime = QDateTime::fromString(minDate, "yyyy-MM-dd");
+    QDateTime maxDateTime = QDateTime::fromString(maxDate, "yyyy-MM-dd");
+    if (!minDateTime.isValid() || !maxDateTime.isValid()) {
+        return false;
+    }
+    // Vérifie si la date minimale est inférieure ou égale à la date maximale
+    return minDateTime <= maxDateTime;
 }
+
 
 // Vérifie la validité de la taille minimale de fichier
 bool validateMinFileSize(qint64 minFileSize) {
@@ -48,13 +75,13 @@ bool validateInput(const QList<Token>& tokens) {
                 isValid = isValid && validateFileName(token.value);
                 break;
             case TokenType::FileType:
-                isValid = isValid && validateFileType(token.value);
+                isValid = isValid && validateFileType(token.value, token.value);
                 break;
             case TokenType::FileExtension:
                 isValid = isValid && validateFileExtension(token.value);
                 break;
             case TokenType::Date:
-                isValid = isValid && validateDate(token.value);
+                isValid = isValid && validateDate(token.value, token.value);
                 break;
             case TokenType::MinFileSize:
                 isValid = isValid && validateMinFileSize(token.value.toLongLong());
